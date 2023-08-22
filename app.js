7ff27e378529a990157560b8e0f602ab2f1836ab
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-08-22 16:18:20 +0000

    image array

---
+++ app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOCMContentLoaded', () =>)
+{
+  //card options
+  const cardArray = 
+    [
+      {
+        name: 'blank',
+        img: 'images/blank.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'fries',
+        img: 'images/fries.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+      },
+      {
+        name: 'pizza',
+        img: 'images/pizza.png'
+      },
+      {
+        name: 'white',
+        img: 'images/white.png'
+      },
+
+      
+    ]
+})
