b779b78a9323585887fbfd57cfd59889113dbb46
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-08-22 16:31:42 +0000

    building gameboard

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ document.addEventListener('DOCMContentLoaded', () =>)
         img: 'images/blank.png'
       },
       {
+        name: 'blank',
+        img: 'images/blank.png'
+      },
+      {
         name: 'cheeseburger',
         img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'fries',
+        img: 'images/fries.png'
       },
       {
         name: 'fries',
@@ -20,8 +32,20 @@ document.addEventListener('DOCMContentLoaded', () =>)
         img: 'images/hotdog.png'
       },
       {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+      },
+      {
         name: 'ice-cream',
         img: 'images/ice-cream.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
       },
       {
         name: 'milkshake',
@@ -32,10 +56,32 @@ document.addEventListener('DOCMContentLoaded', () =>)
         img: 'images/pizza.png'
       },
       {
+        name: 'pizza',
+        img: 'images/pizza.png'
+      },
+      {
         name: 'white',
         img: 'images/white.png'
-      },
-
-      
+      },      
+      {
+        name: 'white',
+        img: 'images/white.png'
+      },      
     ]
+
+  const grid - document.querySeleector('.grid')
+
+  //create you board
+  function createBoard()
+  {
+    for (let i =0; i< cardArray.length; i++)
+    {
+      var card = document.createElement('img')
+      card.setAttribute('src', 'images/blank.png')
+      card.setAttribute('data-id', i)
+//      card.addEventListener('click', flipcard)
+      grid.appendChild(card)
+    }
+  }
+  
 })
